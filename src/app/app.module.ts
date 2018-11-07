02396b0a3c20c382ad87e0f3d835762244c9f0af
Leonardo Jimenez <jimenezleonardop@gmail.com>
2018-11-07 15:13:03 -0400

    Basic Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { TagsComponent } from './tags/tags.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    TagsComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule
